fdf42227cb7d19f5a8a57495098383a2b1ae828c
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-20 15:56:49 +0530

    basename

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = React.memo(() => {
 	return (
 		<div className='App'>
 			<ToastContainer />
-			<BrowserRouter basename="DevQuizzer-Client">
+			<BrowserRouter basename="/DevQuizzer-Client/">
 				<Header />
 				<Routes>
 					<Route path='/' element={<Homepage />} />
